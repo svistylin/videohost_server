c7b0109003ea4b9e725bd8cad66956d374fb6a4e
svistylin <vitalik251781@gmail.com>
2020-02-15 09:42:15 +0200

    Initial commit

---
+++ src/main/java/com/videohost/videohost_server/VideohostServerApplication.java
@@ -0,0 +1,13 @@
+package com.videohost.videohost_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideohostServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideohostServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/videohost/videohost_server/VideohostServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videohost.videohost_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideohostServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
